f864e18230dbed970a8dc63aa7f70238eb30e4c8
Hardik-kahar <dev2-uidai-gil@gujarat.gov.in>
2024-10-30 11:55:11 +0530

    Update Variable.java

--- myapp/core/Variable.java
+++ myapp/core/Variable.java
@@ -1,47 +1,47 @@
 package myapp.core;
 
-public class Variable {
-    static int sa = 30;
-    int a = 20;
+//public class demo {
+//    static int sa = 30;
+//    int a = 20;
 
-    //	public Variable(int aa) {
-    //		this.a= aa;
-    //		System.out.println(" constructor with parameterize ");
-    //	}
-    static void fun() {
-        class local {
-            void fun1() {
-                System.out.println("class method fun1>> call....");
-            }
-        }
-        new local().fun1();
-    }
+//	public Variable(int aa) {
+//		this.a= aa;
+//		System.out.println(" constructor with parameterize ");
+//	}
+//    static void fun() {
+//        class local {
+//            void fun1() {
+//                System.out.println("class method fun1>> call....");
+//            }
+//        }
+//        new local().fun1();
+//    }
+//
+//    public void student() {
+//        this.a = 50;
+//    }
+//
+//    public Variable() {
+//        System.out.println("construction call....");
+//    }
+//
+//    public void demo() {
+//        int demo1 = 70;
+//        System.out.println(" demo:- " + demo1);
+//    }
+//
+//    static public void main(String[] args) {
+//        //new Variable(sa);
+//        Variable vr = new Variable();
+//        vr.demo();
+//        //vr.a=50;
+//        System.out.println(vr.a);
+//    }
+//}
 
-    public void student() {
-        this.a = 50;
-    }
-
-    public Variable() {
-        System.out.println("construction call....");
-    }
-
-    public void demo() {
-        int demo1 = 70;
-        System.out.println(" demo:- " + demo1);
-    }
-
-    static public void main(String[] args) {
-        //new Variable(sa);
-        Variable vr = new Variable();
-        vr.demo();
-        //vr.a=50;
-        System.out.println(vr.a);
-    }
-}
-
-class demo {
+class Variable {
     public static void main(String[] args) {
-
+        System.out.println("test............");
     }
 } 
 
